79ab54df80d939bc040aa6c9b03225449a6caae2
Raffy Alcoriza <jrpalcoriza@outlook.com>
2023-10-14 02:01:29 +0800

    Implement infinitely scrolling character list

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -3,7 +3,26 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const createApolloClient = () => {
   return new ApolloClient({
     uri: "https://rickandmortyapi.com/graphql",
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            characters: {
+              keyArgs: false,
+              merge(existing, incoming) {
+                if (!existing) {
+                  return incoming;
+                }
+                return {
+                  ...incoming,
+                  results: [...existing.results, ...incoming.results],
+                };
+              },
+            },
+          },
+        },
+      },
+    }),
   });
 };
 
