c502e237a60153f8deeaf20f22f8f198b9110db8
Raffy Alcoriza <jrpalcoriza@outlook.com>
2023-10-13 17:36:07 +0800

    Add GraphQL prerequisites and MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+
+import createApolloClient from "@/lib/apollo-client";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = createApolloClient();
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
