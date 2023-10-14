1419b6663c87589388bae7189480d1d18e2eb489
Raffy Alcoriza <jrpalcoriza@outlook.com>
2023-10-14 08:59:10 +0800

    Resolve baseline styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 
 import createApolloClient from "@/lib/apollo-client";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = createApolloClient();
 
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <div className={inter.className}>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </div>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from "@mui/material";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <CssBaseline />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
