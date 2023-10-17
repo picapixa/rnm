f0cc26959b8fcea332187c060bd72eee4ef457ae
Raffy Alcoriza <jrpalcoriza@outlook.com>
2023-10-18 00:43:59 +0800

    Properly log errors in dev

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,12 @@
+import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
 import { CssBaseline } from "@mui/material";
 import { Html, Head, Main, NextScript } from "next/document";
+
+if (process.env.NODE_ENV !== "production") {
+  // Adds messages only in a dev environment
+  loadDevMessages();
+  loadErrorMessages();
+}
 
 export default function Document() {
   return (
